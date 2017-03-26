89fabe047a2e007150cd05a6592c2fc1ce34d122
Martin Armbruster <armbrustermartin@googlemail.com>
2017-03-26 14:05:21 +0200

    Extended the ReplaceJavaTest.

--- src/test/java/marm/src/ana/ReplaceJavaTest.java
+++ src/test/java/marm/src/ana/ReplaceJavaTest.java
@@ -21,12 +21,15 @@
 package marm.src.ana;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import marm.src.ana.internal.ReplaceJava;
 import marm.src.ana.internal.ReplaceJava2;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -36,7 +39,7 @@ import static org.junit.Assert.assertEquals;
  * Test class for the ReplaceJava classes.
  * 
  * @author Martin Armbruster
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  */
 public class ReplaceJavaTest
@@ -49,6 +52,10 @@ public class ReplaceJavaTest
 	 * Saves the location of the test directory.
 	 */
 	private File testDir;
+	/**
+	 * Temporary file for a LoremIpsum java source code file for time measurement.
+	 */
+	private File loremIpsumFile;
 	
 	/**
 	 * Setups all objects for testing.
@@ -58,6 +65,21 @@ public class ReplaceJavaTest
 	{
 		analyzer = new SourceAnalyzer();
 		testDir = new File("target"+File.separator+"test-classes");
+		try
+		{
+			loremIpsumFile = File.createTempFile("LoremIpsum", ".java", new File(testDir, "replaceTest3"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter(loremIpsumFile));
+			String line = readFile(testDir.getPath()+File.separator+"replace3"+File.separator+"LoremIpsum.txt")+"\n";
+			for(int i=0; i<10000; i++)
+			{
+				bw.write(line);
+			}
+			bw.close();
+		}
+		catch(IOException ioExc)
+		{
+			ioExc.printStackTrace();
+		}
 	}
 	
 	/**
@@ -115,6 +137,53 @@ public class ReplaceJavaTest
 	}
 	
 	/**
+	 * Measures time for the ReplaceJava class with a huge file content.
+	 */
+	@Test
+	public void testReplaceWithLoremIpsum()
+	{
+		HashMap<String, String> replacements = new HashMap<>();
+		replacements.put("dolor", "Something else\\.");
+		replacements.put("Lorem ipsum", "Blablub");
+		replacements.put("sit", "Lorem ipsum");
+		replacements.put("no", "Test 5149");
+		replacements.put(" ", "   ");
+		replacements.put("At", "at");
+		replacements.put("sed", "aber");
+		replacements.put("eir", "eier");
+		replacements.put(",", "");
+		replacements.put("et", "und");
+		analyzer.addSrcFileHandler(new ReplaceJava(replacements));
+		long time = System.nanoTime();
+		analyzer.analyze(new File(testDir, "replaceTest3"));
+		System.out.println("Time for the LoremIpsum replacing using regular expressions: "+(System.nanoTime()-time));
+	}
+	
+	/**
+	 * Measures time for the ReplaceJava2 class with a huge file content.
+	 */
+	@Test(timeout = 30000)
+	public void testReplace2WithLoremIpsum()
+	{
+		HashMap<String, String> replacements = new HashMap<>();
+		replacements.put("dolor", "Something else.");
+		replacements.put("Lorem ipsum", "Blablub");
+		replacements.put("sit", "Lorem ipsum");
+		replacements.put("no", "Test 5149");
+		replacements.put(" ", "   ");
+		replacements.put("At", "at");
+		replacements.put("sed", "aber");
+		replacements.put("eir", "eier");
+		replacements.put(",", "");
+		replacements.put("et", "und");
+		analyzer.addSrcFileHandler(new ReplaceJava2(replacements));
+		long time = System.nanoTime();
+		analyzer.analyze(new File(testDir, "replaceTest3"));
+		System.out.println("Time for the LoremIpsum replacing using the Aho-Corasick algorithm: "
+				+(System.nanoTime()-time));
+	}
+	
+	/**
 	 * Reads the content of a file.
 	 * 
 	 * @param file path of the file.
@@ -139,6 +208,18 @@ public class ReplaceJavaTest
 		catch(IOException ioExc)
 		{
 			return "";
+		}
+	}
+	
+	/**
+	 * Cleans everything.
+	 */
+	@After
+	public void tearDown()
+	{
+		if(loremIpsumFile!=null)
+		{
+			loremIpsumFile.delete();
 		}
 	}
 }
